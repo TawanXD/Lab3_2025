33b69950aae3225015dc40f2abff1ce7f39d7f44
Suchanun <suchanun_sir@cmu.ac.th>
2025-08-08 02:51:02 +0700

    done 3 naaa

---
+++ src/views/NetworkErroeView.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { defineProps, withDefaults } from 'vue'
+
+
+// const props = withDefaults(
+//     defineProps<{
+//         resource: string
+//     }>(),
+//     {
+//         resource: 'page'
+//     }
+// )
+</script>
+
+
+
+<template>
+    <div class="networEerror">
+    <h1>Uh-Oh!</h1>
+    
+    <h3> It looks like you're expreriencing some network issues naaaaa
+        <a href="#" @click="$router.go(-1)">Go back</a> to try agian.
+    </h3>
+    </div>
+</template>
