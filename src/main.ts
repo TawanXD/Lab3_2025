fea00556dfa2732819ec9990b5884c468bdaf6b8
Suchanun <suchanun_sir@cmu.ac.th>
2025-08-08 09:29:52 +0700

    Lab5 1.7

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { createApp } from "vue"
 import App from "./App.vue"
 import { createPinia } from 'pinia'
 import { router } from "./router" 
+import './assets/tailwind.css'
+import './assets/style.css'
 import 'nprogress/nprogress.css'
 //import "./assets/main.css"
 
